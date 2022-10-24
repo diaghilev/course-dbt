c82f4b28c6e918fa1ad4d6565baf790fe4b71e21
diaghilev <hi@laukaye.com>
2022-10-24 22:33:12 +0000

    Adjusted event aggregation for better readability in the future

--- greenery/models/marts/Product/int_sessions_agg.sql
+++ greenery/models/marts/Product/int_sessions_agg.sql
@@ -9,20 +9,14 @@ WITH events AS (
     SELECT * FROM {{ ref('stg_events') }}
 )
 
-{%-
-  set event_types = dbt_utils.get_column_values(
-    table = ref('stg_events'),
-        column = 'event_type',
-            order_by = 'event_type asc'
-  )
--%}
-
+-- Hard coding event types has downsides, but this way I could rename and order the resulting columns. While a macro wasn't necessary here, it was good practice!
 SELECT
-    session_id
-    , user_id
-    {%- for event_type in event_types %}
-    , sum(case when event_type = '{{ event_type }}' then 1 else 0 end) as {{event_type}}s
-    {%- endfor %}
-    , CASE WHEN checkouts = 0 THEN 0 ELSE 1 END AS has_converted
+    session_id,
+    user_id,
+    {{ agg_event_type('page_view') }} AS session_page_views,
+    {{ agg_event_type('add_to_cart') }} AS session_add_to_carts,
+    {{ agg_event_type('checkout') }} AS session_checkouts,
+    {{ agg_event_type('package_shipped') }} AS session_package_shippeds,
+    CASE WHEN session_checkouts = 0 THEN 0 ELSE 1 END AS has_converted
 FROM events
 GROUP BY 1,2
